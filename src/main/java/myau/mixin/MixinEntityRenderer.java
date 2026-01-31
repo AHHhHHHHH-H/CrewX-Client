2047df5e4656c1036f5afea04a6e4734e0f32551
CrowlyUF <azsauloferreira89@gmail.com>
2026-01-31 13:22:39 -0300

    Atualizar o MixinEntityRenderer.java

--- src/main/java/myau/mixin/MixinEntityRenderer.java
+++ src/main/java/myau/mixin/MixinEntityRenderer.java
@@ -1,7 +1,6 @@
 package myau.mixin;
 
 import myau.Myau;
-import myau.data.Box;
 import myau.event.EventManager;
 import myau.events.PickEvent;
 import myau.events.RaytraceEvent;
@@ -32,11 +31,11 @@ import java.util.List;
 @Mixin(value = {EntityRenderer.class}, priority = 9999)
 public abstract class MixinEntityRenderer {
     @Unique
-    private Box<Integer> slot = null;
+    private Integer slotValue = null;
     @Unique
-    private Box<ItemStack> using = null;
+    private ItemStack usingValue = null;
     @Unique
-    private Box<Integer> useCount = null;
+    private Integer useCountValue = null;
     @Shadow
     private Minecraft mc;
     @Shadow
@@ -52,15 +51,15 @@ public abstract class MixinEntityRenderer {
             if (scaffold.isEnabled() && scaffold.itemSpoof.getValue()) {
                 int slot = scaffold.getSlot();
                 if (slot >= 0) {
-                    this.slot = new Box<>(this.mc.thePlayer.inventory.currentItem);
+                    this.slotValue = this.mc.thePlayer.inventory.currentItem;
                     this.mc.thePlayer.inventory.currentItem = slot;
                 }
             }
             KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
             if (killAura.isEnabled() && killAura.isBlocking()) {
-                this.using = new Box<>(((IAccessorEntityPlayer) this.mc.thePlayer).getItemInUse());
+                this.usingValue = ((IAccessorEntityPlayer) this.mc.thePlayer).getItemInUse();
                 ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUse(this.mc.thePlayer.inventory.getCurrentItem());
-                this.useCount = new Box<>(((IAccessorEntityPlayer) this.mc.thePlayer).getItemInUseCount());
+                this.useCountValue = ((IAccessorEntityPlayer) this.mc.thePlayer).getItemInUseCount();
                 ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUseCount(69000);
             }
         }
@@ -71,17 +70,17 @@ public abstract class MixinEntityRenderer {
             at = {@At("RETURN")}
     )
     private void postUpdateCameraAndRender(float float1, long long2, CallbackInfo callbackInfo) {
-        if (this.slot != null) {
-            this.mc.thePlayer.inventory.currentItem = this.slot.value;
-            this.slot = null;
+        if (this.slotValue != null) {
+            this.mc.thePlayer.inventory.currentItem = this.slotValue;
+            this.slotValue = null;
         }
-        if (this.using != null) {
-            ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUse(this.using.value);
-            this.using = null;
+        if (this.usingValue != null) {
+            ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUse(this.usingValue);
+            this.usingValue = null;
         }
-        if (this.useCount != null) {
-            ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUseCount(this.useCount.value);
-            this.useCount = null;
+        if (this.useCountValue != null) {
+            ((IAccessorEntityPlayer) this.mc.thePlayer).setItemInUseCount(this.useCountValue);
+            this.useCountValue = null;
         }
     }
 
@@ -94,7 +93,7 @@ public abstract class MixinEntityRenderer {
         if (scaffold.isEnabled() && scaffold.itemSpoof.getValue()) {
             int slot = scaffold.getSlot();
             if (slot >= 0) {
-                this.slot = new Box<>(this.mc.thePlayer.inventory.currentItem);
+                this.slotValue = this.mc.thePlayer.inventory.currentItem;
                 this.mc.thePlayer.inventory.currentItem = slot;
             }
         }
@@ -103,7 +102,7 @@ public abstract class MixinEntityRenderer {
         if (autoBlockIn.isEnabled() && autoBlockIn.itemSpoof.getValue()) {
             int slot = autoBlockIn.getSlot();
             if (slot >= 0) {
-                this.slot = new Box<>(this.mc.thePlayer.inventory.currentItem);
+                this.slotValue = this.mc.thePlayer.inventory.currentItem;
                 this.mc.thePlayer.inventory.currentItem = slot;
             }
         }
@@ -114,9 +113,9 @@ public abstract class MixinEntityRenderer {
             at = {@At("RETURN")}
     )
     private void postUpdateRenderer(CallbackInfo callbackInfo) {
-        if (this.slot != null) {
-            this.mc.thePlayer.inventory.currentItem = this.slot.value;
-            this.slot = null;
+        if (this.slotValue != null) {
+            this.mc.thePlayer.inventory.currentItem = this.slotValue;
+            this.slotValue = null;
         }
     }
 
